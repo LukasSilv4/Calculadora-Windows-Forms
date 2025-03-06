368eeb16addd7eb3b54b7051031e0b89af8e8e16
Lukassilv4 <lukascavalcante78@gmail.com>
2025-03-05 23:25:08 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1 teste calculadora/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace WinFormsApp1_teste_calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            btnUm = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlLightLight;
+            textBox1.Location = new Point(9, 12);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(230, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.HotTrack;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(190, 266);
+            button1.Name = "button1";
+            button1.Size = new Size(52, 67);
+            button1.TabIndex = 2;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.HotTrack;
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(190, 201);
+            button2.Name = "button2";
+            button2.Size = new Size(52, 59);
+            button2.TabIndex = 3;
+            button2.Text = "-";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.HotTrack;
+            button3.ForeColor = SystemColors.Control;
+            button3.Location = new Point(190, 131);
+            button3.Name = "button3";
+            button3.Size = new Size(52, 64);
+            button3.TabIndex = 4;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.HotTrack;
+            button4.ForeColor = SystemColors.Control;
+            button4.Location = new Point(190, 67);
+            button4.Name = "button4";
+            button4.Size = new Size(52, 58);
+            button4.TabIndex = 5;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Window;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(2, 17);
+            label1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ScrollBar;
+            button5.Location = new Point(12, 67);
+            button5.Name = "button5";
+            button5.Size = new Size(54, 58);
+            button5.TabIndex = 7;
+            button5.Text = "7";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ScrollBar;
+            button6.Location = new Point(72, 67);
+            button6.Name = "button6";
+            button6.Size = new Size(55, 58);
+            button6.TabIndex = 8;
+            button6.Text = "8";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ScrollBar;
+            button7.Location = new Point(133, 67);
+            button7.Name = "button7";
+            button7.Size = new Size(51, 58);
+            button7.TabIndex = 9;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ScrollBar;
+            button8.Location = new Point(12, 131);
+            button8.Name = "button8";
+            button8.Size = new Size(54, 64);
+            button8.TabIndex = 10;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ScrollBar;
+            button9.Location = new Point(72, 131);
+            button9.Name = "button9";
+            button9.Size = new Size(55, 64);
+            button9.TabIndex = 11;
+            button9.Text = "5";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ScrollBar;
+            button10.Location = new Point(133, 131);
+            button10.Name = "button10";
+            button10.Size = new Size(51, 64);
+            button10.TabIndex = 12;
+            button10.Text = "6";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // btnUm
+            // 
+            btnUm.BackColor = SystemColors.ScrollBar;
+            btnUm.Location = new Point(72, 201);
+            btnUm.Name = "btnUm";
+            btnUm.Size = new Size(55, 59);
+            btnUm.TabIndex = 13;
+            btnUm.Text = "1";
+            btnUm.UseVisualStyleBackColor = false;
+            btnUm.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = SystemColors.ScrollBar;
+            button12.Location = new Point(133, 201);
+            button12.Name = "button12";
+            button12.Size = new Size(51, 59);
+            button12.TabIndex = 14;
+            button12.Text = "2";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.ScrollBar;
+            button13.Location = new Point(12, 201);
+            button13.Name = "button13";
+            button13.Size = new Size(54, 59);
+            button13.TabIndex = 15;
+            button13.Text = "3";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ScrollBar;
+            button14.Location = new Point(72, 266);
+            button14.Name = "button14";
+            button14.Size = new Size(55, 67);
+            button14.TabIndex = 16;
+            button14.Text = "0";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.HotTrack;
+            button15.ForeColor = SystemColors.Control;
+            button15.Location = new Point(12, 41);
+            button15.Name = "button15";
+            button15.Size = new Size(54, 23);
+            button15.TabIndex = 17;
+            button15.Text = "C";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.HotTrack;
+            button16.ForeColor = SystemColors.Control;
+            button16.Location = new Point(133, 266);
+            button16.Name = "button16";
+            button16.Size = new Size(51, 67);
+            button16.TabIndex = 18;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.HotTrack;
+            button17.ForeColor = SystemColors.Control;
+            button17.Location = new Point(12, 266);
+            button17.Name = "button17";
+            button17.Size = new Size(54, 67);
+            button17.TabIndex = 19;
+            button17.Text = ",";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(251, 356);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(btnUm);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button btnUm;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+    }
+}
---
+++ WinFormsApp1 teste calculadora/Form1.cs
@@ -0,0 +1,195 @@
+namespace WinFormsApp1_teste_calculadora
+{
+    public partial class Form1 : Form
+    {
+      
+        private decimal Resultado { get; set; }
+        private decimal Valor { get; set; }
+        private Operacao OperacaoSelecionada { get; set; }
+        private enum Operacao
+        {
+            Adicao,
+            Subtracao,
+            Multiplicacao,
+            Divisao
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+
+                textBox1.Text = "";
+                return;
+            }
+            OperacaoSelecionada = Operacao.Adicao;
+            Valor = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = "";
+            label1.Text = "+";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+               
+                textBox1.Text = "";
+                return;
+            }
+            OperacaoSelecionada = Operacao.Subtracao;
+            Valor = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = "";
+            label1.Text = "-";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+
+                textBox1.Text = "";
+                return;
+            }
+            OperacaoSelecionada = Operacao.Multiplicacao;
+            Valor = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = "";
+            label1.Text = " X";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+
+                textBox1.Text = "";
+                return;
+            }
+            OperacaoSelecionada = Operacao.Divisao;
+            Valor = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = "";
+            label1.Text = "/";
+           
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+
+
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            label1.Text = " ";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+
+                textBox1.Text = "";
+                return;
+            }
+
+            switch (OperacaoSelecionada)
+            {
+            
+                case Operacao.Adicao:
+                    Resultado = Valor + Convert.ToDecimal(textBox1.Text);
+                    break;
+                case Operacao.Subtracao:
+                    Resultado = Valor - Convert.ToDecimal(textBox1.Text);
+                    break;
+                case Operacao.Multiplicacao:
+                    Resultado = Valor * Convert.ToDecimal(textBox1.Text);
+                    break;
+                case Operacao.Divisao:
+                    if (Convert.ToDecimal(textBox1.Text) == 0)
+                    {
+                        textBox1.Text = "Não é Possível Dividir por Zero";
+                        label1.Text = "";
+                        return; 
+                    }
+                    Resultado = Valor / Convert.ToDecimal(textBox1.Text);
+                    break;
+            }
+            
+            Resultado = Valor / Convert.ToDecimal(textBox1.Text);
+                    textBox1.Text = Convert.ToString(Resultado);
+            label1.Text = "=";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(","))
+                textBox1.Text += " , ";
+
+        }
+    }
+
+
+
+}
